004c6ba856fe9860f93ed89c86257cbe96680c3a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2025-03-27 16:28:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
-import Loginscreen from "./Loginscreen"; // Import Loginscreen component
-import Profile from "./Profile"; // Import Profile component
+import Loginscreen from "./Loginscreen"; 
+import Profile from "./Profile"; 
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Loginscreen />} /> {/* Route for Loginscreen */}
-        {/* Route for Profile */}
+        
         <Route path="/profile" element={<Profile />} /> {/* Route for Profile */}
       </Routes>
     </Router>
